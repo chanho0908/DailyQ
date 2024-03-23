069bfa5ec6c242ae71a4e5f6080474c7c26d7eaf
chanho <uni070@naver.com>
2024-03-23 19:38:00 +0900

    first commit

---
+++ app/src/main/java/online/dailyq/ui/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package online.dailyq.ui.base
+
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import online.dailyq.R
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/online/dailyq/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package online.dailyq.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import online.dailyq.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
