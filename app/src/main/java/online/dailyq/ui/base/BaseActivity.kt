6e5ad267666b6ccc605f8b8721c42c35d6d18977
chanho <uni070@naver.com>
2024-03-23 19:48:54 +0900

    2장 완료

--- app/src/main/java/online/dailyq/ui/base/BaseActivity.kt
+++ app/src/main/java/online/dailyq/ui/base/BaseActivity.kt
@@ -1,10 +1,28 @@
 package online.dailyq.ui.base
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import online.dailyq.R
+import online.dailyq.databinding.ActivityMainBinding
 
 abstract class BaseActivity: AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        with(binding){
+            navView.setOnItemSelectedListener {
+                TODO()
+            }
+
+            navView.selectedItemId = R.id.today
+        }
+    }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
--- app/src/main/java/online/dailyq/ui/timeline/TimeLineFragment.kt
+++ app/src/main/java/online/dailyq/ui/timeline/TimeLineFragment.kt
@@ -6,17 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import online.dailyq.R
+import online.dailyq.databinding.FragmentTimeLineBinding
 
 
 class TimeLineFragment : Fragment() {
-    private var _binding: TimeLineFragmentBinding ?= null
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_line, container, false)
+    private var _binding: FragmentTimeLineBinding ?= null
+    private val binding get() = _binding!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentTimeLineBinding.inflate(layoutInflater)
+        return binding.root
     }
 
 }
